dc58c5bb236ff1ecedc70d1ce7f4b4d4f5f01b58
Katie <78595877+katie-bw@users.noreply.github.com>
2024-05-02 11:30:17 +0100

    Update __init__.py

--- kvb/__init__.py
+++ kvb/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ["LECSS", "data_processing", "figures", "skinsen"]
 
-from kvb import LECSS
-from kvb import data_processing 
-from kvb import figures 
-from kvb import skinsen
+import LECSS
+import data_processing 
+import figures 
+import skinsen
