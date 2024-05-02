9d3768bfccd2957606d39db809c605e3f9a2708f
Katie <78595877+katie-bw@users.noreply.github.com>
2024-05-02 11:09:54 +0100

    Update __init__.py

--- kvb/__init__.py
+++ kvb/__init__.py
@@ -1,1 +1,5 @@
 
+https://github.com/katie-bw/kvb/edit/main/kvb/LECSS.py
+https://github.com/katie-bw/kvb/edit/main/kvb/data_processing.py
+https://github.com/katie-bw/kvb/edit/main/kvb/figures.py
+https://github.com/katie-bw/kvb/edit/main/kvb/skinsen.py
